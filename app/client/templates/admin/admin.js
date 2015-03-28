f89224fb273c21400019ccca82e7c6a85d79fe5f
atrotter <andrew.trotter@mail.utoronto.ca>
2015-03-27 23:53:26 -0400

    Added a js handler for the new admin form.

--- app/client/templates/admin/admin.js
+++ app/client/templates/admin/admin.js
@@ -1,3 +1,35 @@
-Template.dashboard.rendered = function() {
-
-};
+Template.admin.events({
+	"submit .new-question": function (event) {
+		var domain = parseInt(event.target.domain.value, 10);
+		var subdomain = parseInt(event.target.subdomain.value, 10);
+		var level = parseInt(event.target.level.value, 10);
+		var section = parseInt(event.target.section.value, 10);
+		var title = event.target.title.value;
+		var problem = event.target.problem.value;
+		var code = split(event.target.code.value, "\n");
+		
+		Challenges.insert( {
+			domain: domain,
+			subdomain: subdomain,
+			level: level,
+			section: section,
+			title: title,
+			problem: problem,
+			code: code,
+			createdAt: new Date() // current time
+		});
+		
+		//clear fields
+		event.target.domain.value = "";
+		event.target.subdomain.value = "";
+		event.target.level.value = "";
+		event.target.section.value = "";
+		event.target.title.value = "";
+		event.target.problem.value = "";
+		event.target.code.value = "";
+		
+		console.log("New question inserted into database.");
+		return false;
+	}
+});
+		
